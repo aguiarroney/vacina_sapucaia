7a77e6d5f5269a84444d7ee11894e39f65a79d81
Roney Aguiar <raguiar@muxi.corp>
2021-06-27 17:11:06 -0300

    adding intent to notification

--- app/src/main/java/com/example/vacinasapucaia/utils/NotificationUtils.kt
+++ app/src/main/java/com/example/vacinasapucaia/utils/NotificationUtils.kt
@@ -1,9 +1,12 @@
 package com.example.vacinasapucaia.repository
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.example.vacinasapucaia.MainActivity
 import com.example.vacinasapucaia.R
 
 
@@ -11,12 +14,24 @@ private val NOTIFICATION_ID = 0
 
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     Log.i("notification", "sender")
+
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
     val builder = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.notification_channel_id)
     ).setSmallIcon(R.drawable.ic_vaccine)
         .setContentTitle(applicationContext.getString(R.string.app_name))
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
     notify(NOTIFICATION_ID, builder.build())
 }
