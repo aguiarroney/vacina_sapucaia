8e57beb4a544099c08950ab3317684eb1ef4f5cf
Roney Aguiar <raguiar@muxi.corp>
2021-07-10 11:36:15 -0300

    adjusting retrofit instance for using only one endpoing

--- app/src/main/java/com/example/vacinasapucaia/api/ApiService.kt
+++ app/src/main/java/com/example/vacinasapucaia/api/ApiService.kt
@@ -5,6 +5,9 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET(".")
+    @GET("comunicado/2ccee441-4256-483b-9898-0307284a3ad9/")
     suspend fun getPage(): Response<String>
+
+    @GET("boletim/covid-19/")
+    suspend fun getPage2(): Response<String>
 }
